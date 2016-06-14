9d93d2a09dee0ae4faddddb913e28853c914ac2f
more-like-a-puma <jessicamaycreative@gmail.com>
2016-06-14 21:11:49 +1000

    fixing up the :through association

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,5 +1,5 @@
 class Flight < ActiveRecord::Base
   belongs_to :plane
   has_many :reservations
-  has_many :users, through :reservations
+  has_many :users, through: :reservations
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :email, :presence => true
   has_many :reservations
-  has_many :flights, through :reservations
+  has_many :flights, through: :reservations
 end
