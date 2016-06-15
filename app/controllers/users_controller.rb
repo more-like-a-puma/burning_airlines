2c20ecba2141c62002c009b4262d35a45ee54525
more-like-a-puma <jessicamaycreative@gmail.com>
2016-06-15 22:10:22 +1000

    updating admin rights again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+  before_action :authorize, :except => [:show, :new, :edit, :create, :update, :destroy]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
   # GET /users.json
   def index
-    @users = User.all 
+    @users = User.all
   end
 
   # GET /users/1
