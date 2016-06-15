cc569a4b14495e088c9c875c8661ad905e8d34a9
more-like-a-puma <jessicamaycreative@gmail.com>
2016-06-16 09:39:25 +1000

    adding flightnum column to flights table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160614090153) do
+ActiveRecord::Schema.define(version: 20160615233804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160614090153) do
     t.integer  "plane_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "flightnum"
   end
 
   create_table "planes", force: :cascade do |t|
