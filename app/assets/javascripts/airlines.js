29453d22e367aebfcf79ae5e20d038279c1d851c
Jane Hogan <janeodarling@gmail.com>
2016-06-16 15:32:07 +1000

    Putting the JS view plane into the stuff

--- app/assets/javascripts/airlines.js
+++ app/assets/javascripts/airlines.js
@@ -1,6 +1,22 @@
+
+
 $(document).ready(function() {
+  app.reservations = new app.ReservationsCollection();
 
     var flightRouter = new app.planesRouter();
     Backbone.history.start();
 
+    // Wait until the secretsCollection has finished fetching
+    app.reservations.fetch();
+
+    // Polling
+    window.setInterval(function () {
+      app.reservations.fetch();
+    }, 1000);
+
+
 });
+
+
+
+// });
